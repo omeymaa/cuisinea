ee928cff62405ffd1b0f6174eab28a695c2ec5d1
omeymaa <omeyma.maatoug@gmail.com>
2023-02-28 15:12:56 +0100

    modifications page Nos recettes

--- recettes.php
+++ recettes.php
@@ -1,6 +1,13 @@
 <?php
 require_once('templates/header.php');
 require_once('lib/recipe.php');
+
+$sql = 'SELECT * FROM recipes ORDER BY id DESC';
+
+$query = $pdo->prepare($sql);
+$query->execute();
+$recipes = $query->fetchAll();
+
 ?>
 
 
