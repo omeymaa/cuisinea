9154766f6a498ba823d9b314c7ab8945fc218ad6
omeymaa <omeyma.maatoug@gmail.com>
2023-02-28 14:53:59 +0100

    creation de fonctions et amelioration du code

--- recette.php
+++ recette.php
@@ -5,31 +5,18 @@ require_once('lib/recipe.php');
 
 $pdo = new PDO('mysql:host=localhost;dbname=cuisinea;charset=utf8mb4;', 'root', 'root');
 
-
 $id = $_GET['id'];
-
-$query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
-$query->bindParam(':id', $id, PDO::PARAM_INT);
-$query->execute();
-$recipe = $query->fetch();
+$recipe = getRecipeById($pdo, $id);
 
 if ($recipe) {
-
-    if ($recipe['image'] === null) {
-        $imagePath = _ASSETS_IMG_PATH_.'recipe_default.jpg';
-    } else {
-        $imagePath = _RECIPES_IMG_PATH_.$recipe['image'];
-    };
-    
     $ingredients = linesToArray($recipe['ingredients']);
     $instructions = linesToArray($recipe['instructions']);
-    
 ?>
 
 
 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
     <div class="col-10 col-sm-8 col-lg-6">
-        <img src="<?=$imagePath;?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+        <img src="<?=getRecipeImage($recipe['image']);?>" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
     </div>
     <div class="col-lg-6">
         <h1 class="display-5 fw-bold lh-1 mb-3"><?=$recipe['title'];?></h1>
